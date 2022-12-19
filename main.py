f088ffd458c764c53b2047fe39184783f430324b
estuardo8u14 <estuaro8u14@gmail.com>
2022-12-19 14:54:07 -0600

    xcv

---
+++ main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI, Request
+from pydantic import BaseModel
+
+app = FastAPI()
+
+
+class Item(BaseModel):
+    tiempo: str
+    exchange: str
+    ticker: str
+    texto: str
+
+@app.get("/")
+async def root():
+    return {"message": "Recivido Exitosamente"}
+
+@app.post("/alerta")
+async def alerta(request: Request):
+    msg = request.json
+    tiempo = msg['time']
+    exchange = msg['exchange']
+    ticker = msg['ticker']
+    texto = msg['texto']
+    print(tiempo)
+    print(exchange)
+    print(ticker)
+    print(texto)
+
+
+
+    return await {
+        'code': 'Exitosa',
+        'msg': request.json()
+    }
+    
